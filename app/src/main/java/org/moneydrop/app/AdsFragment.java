85bd1396b11f2f86fb53df0403f157ddd84a471d
Muztahid Durjoy <muztahiddurjoy99@gmail.com>
2021-12-19 14:16:06 +0600

    init

---
+++ app/src/main/java/org/moneydrop/app/AdsFragment.java
@@ -0,0 +1,80 @@
+package org.moneydrop.app;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.startapp.sdk.adsbase.Ad;
+import com.startapp.sdk.adsbase.StartAppAd;
+import com.startapp.sdk.adsbase.adlisteners.AdEventListener;
+import com.startapp.sdk.adsbase.adlisteners.VideoListener;
+
+
+public class AdsFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public AdsFragment() {
+
+    }
+
+
+    public static AdsFragment newInstance(String param1, String param2) {
+        AdsFragment fragment = new AdsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+
+    private Button showbtn;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_ads, container, false);
+        showbtn = root.findViewById(R.id.showbutton);
+        showbtn.setOnClickListener((v)->{
+            final StartAppAd appAd = new StartAppAd(getActivity());
+            appAd.setVideoListener(new VideoListener() {
+                @Override
+                public void onVideoCompleted() {
+                    Toast.makeText(getActivity(), "Video Finished", Toast.LENGTH_SHORT).show();
+                }
+            });
+            appAd.loadAd(StartAppAd.AdMode.REWARDED_VIDEO, new AdEventListener() {
+                @Override
+                public void onReceiveAd(@NonNull Ad ad) {
+                    appAd.showAd();
+                }
+
+                @Override
+                public void onFailedToReceiveAd(@Nullable Ad ad) {
+                    Toast.makeText(getActivity(), ad.errorMessage.toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        });
+        return root;
+    }
+}
---
+++ app/src/main/java/org/moneydrop/app/Task2aActivity.java
@@ -0,0 +1,14 @@
+package org.moneydrop.app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Task2aActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task2a);
+    }
+}
